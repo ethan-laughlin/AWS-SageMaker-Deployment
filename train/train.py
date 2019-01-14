01e797ec6c3d2e6fc3d4e8824b7fe46094696740
locklin-e <elocklin.ai@gmail.com>
2019-01-13 21:29:58 -0600

    feat: Add training code to Repo

--- train/train.py
+++ train/train.py
@@ -68,8 +68,33 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     """
     
     # TODO: Paste the train() method developed in the notebook here.
-
-    pass
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: Complete this train method to train the model provided.
+            
+            # Forward Pass
+            model.zero_grad()
+            output = model(batch_X)
+            
+            # Utilize clip_grad_norm to avoid any exploding gradient problems
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 5) # Starting with 5 as this has worked well on previous RNNs
+            
+            # Perform backprop & optimization
+            loss = loss_fn(output, batch_y)
+            loss.backward()
+            
+            # Perform backprop
+            optimizer.step()            
+            
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 
 if __name__ == '__main__':
